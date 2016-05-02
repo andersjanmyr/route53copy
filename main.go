abc8afd39d09b1ed925c21c43bc3a7b96b4b5790
Jan Nabbefeld <jan.nabbefeld@kreuzwerker.de>
2016-05-02 14:45:52 +0200

    added pagination contributed by @pcorliss

--- main.go
+++ main.go
@@ -80,11 +80,26 @@ func getResourceRecords(profile string, domain string) ([]*route53.ResourceRecor
 	params := &route53.ListResourceRecordSetsInput{
 		HostedZoneId: aws.String(*zone.Id),
 	}
-	resp, err := service.ListResourceRecordSets(params)
-	if err != nil {
-		return nil, err
+	var rrsets []*route53.ResourceRecordSet
+
+	for {
+		var resp *route53.ListResourceRecordSetsOutput
+		resp, err = service.ListResourceRecordSets(params)
+
+		if err != nil {
+			return nil, err
+		}
+
+		rrsets = append(rrsets, resp.ResourceRecordSets...)
+		if *resp.IsTruncated {
+			params.StartRecordName = resp.NextRecordName
+			params.StartRecordType = resp.NextRecordType
+			params.StartRecordIdentifier = resp.NextRecordIdentifier
+		} else {
+			break
+		}
 	}
-	return resp.ResourceRecordSets, nil
+	return rrsets, nil
 }
 
 func createChanges(srcDomain string, destDomain string, recordSets []*route53.ResourceRecordSet) []*route53.Change {
